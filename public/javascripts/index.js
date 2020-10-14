126b18eab38fb8088a9935692fb061131d8f4935
Ryan Lynch <ReLynch91@gmail.com>
2020-10-13 19:39:49 -0700

    dom javascript work

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,34 +2,34 @@ const axios = require('axios');
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    let isbn = '0201558025';
-    axios.get(`/books/${isbn}`)
-    .then((response) => {
-        console.log(response); 
-    })
-    .catch(function (error) {
-        console.log(error);
-    });
+    // let isbn = '0201558025';
+    // axios.get(`/books/${isbn}`)
+    // .then((response) => {
+    //     console.log(response); 
+    // })
+    // .catch(function (error) {
+    //     console.log(error);
+    // });
 
-    let query = "grace hopper";
-    axios.get(`/search?string=${query}`)
-    .then((response) => {
-        console.log(response);
-    })
-    .catch(function (error) {
-        console.log(error);
-    });
+    // let query = "grace hopper";
+    // axios.get(`/search?string=${query}`)
+    // .then((response) => {
+    //     console.log(response);
+    // })
+    // .catch(function (error) {
+    //     console.log(error);
+    // });
     
     // window.add
-    let long, lat;
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(position => {
-            long = position.coords.longitude;
-            lat = position.coords.latitude;
-            console.log(long);
-            console.log(lat);
-        })
-    }
+    // let long, lat;
+    // if (navigator.geolocation) {
+    //     navigator.geolocation.getCurrentPosition(position => {
+    //         long = position.coords.longitude;
+    //         lat = position.coords.latitude;
+    //         console.log(long);
+    //         console.log(lat);
+    //     })
+    // }
 })
 
 document.addEventListener("submit", myFunction);
