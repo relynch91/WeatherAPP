d500c27f54fdc0f596e3e6888a10f01247c7261b
Ryan Lynch <ReLynch91@gmail.com>
2020-11-04 10:50:36 -0800

    add key work

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const fetch = require('node-fetch');
+const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 8000; // process.env accesses heroku's environment variables
 
 app.use(express.static('public'))
@@ -11,12 +12,14 @@ app.get('/', (request, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'))
 });
 
-// app.get('/location/:formattedstuff', (request, res) => {
-//   let gAPIKey = process.env.GOOGLE_GEO_CODING;
-//   let formattedstuff = request.params.formatted;
-//   let latLong = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${formattedstuff}&key=${gAPIKey}`);
-//   return res.send(JSON.stringify(latLong)
-// });
+app.get('/location/getHeroku', (request, res) => {
+  let gAPIKey = process.env.GOOGLE_GEO_CODING;
+  
+  return(res.send(JSON.stringify(gAPIKey)));
+  // let formattedstuff = request.params.formatted;
+  // let latLong = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${formattedstuff}&key=${gAPIKey}`);
+  // return res.send(JSON.stringify(latLong)
+});
 
 
 app.listen(PORT, () => {
