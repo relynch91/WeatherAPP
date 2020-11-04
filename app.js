a4c1ff24f80a5a00571fde4b8f7791f50118dd19
Ryan Lynch <ReLynch91@gmail.com>
2020-11-04 13:52:30 -0800

    add files

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const path = require('path');
 const fetch = require('node-fetch');
 const dotenv = require('dotenv').config()
-const PORT = process.env.PORT || 8000; // process.env accesses heroku's environment variables
+const PORT = process.env.PORT || 8000;
 
 app.use(express.static('public'))
 
@@ -14,11 +14,7 @@ app.get('/', (request, res) => {
 
 app.get('/location/getHeroku', (request, res) => {
   let gAPIKey = process.env.GOOGLE_GEO_CODING;
-  
   return(res.send(JSON.stringify(gAPIKey)));
-  // let formattedstuff = request.params.formatted;
-  // let latLong = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${formattedstuff}&key=${gAPIKey}`);
-  // return res.send(JSON.stringify(latLong)
 });
 
 
